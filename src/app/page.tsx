1f0ef7a911ac1e7460f0e00b98233e335af21df2
Aaron Agarunov <agarunov.aaron@gmail.com>
2024-05-10 00:51:53 -0400

    Add homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
+import dynamic from 'next/dynamic';
+import './globals.css';
+
+// `react-globe.gl` is not compatible with SSR
+const Globe = dynamic(() => import('@/lib/globe'), {
+  ssr: false
+});
+
 export default function Home() {
-  return <main role="main"></main>;
+  return (
+    <main role="main">
+      <Globe />
+    </main>
+  );
 }
