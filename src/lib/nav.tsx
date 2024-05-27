18ff73048913782a746ff6cd16d7f120f06da691
Aaron Agarunov <agarunov.aaron@gmail.com>
2024-05-27 15:24:01 -0400

    Add responsive styles

--- src/lib/nav.tsx
+++ src/lib/nav.tsx
@@ -1,31 +1,31 @@
 import Link from 'next/link';
-import { AlbumList } from '../types/albums';
+import { AlbumList, AlbumTitle } from '../types/albums';
 import React from 'react';
+import { GlobeIcon, InfoIcon, SocialIcon } from './icons';
 
-export const Nav: React.FC<{ slug: string; albums: AlbumList }> = ({
-  albums,
-  slug,
-  ...props
-}) => {
+export const Nav: React.FC<{
+  title?: AlbumTitle;
+  albums: AlbumList;
+}> = ({ albums, title = '', ...props }) => {
   return (
-    <nav className="text-lg" {...props}>
-      <h1 className="font-bold mb-5">
-        <Link href="/">Aaron Agarunov</Link>
+    <nav className="text-lg max-sm:!text-2xl" {...props}>
+      <h1 className="mb-10 max-sm:flex max-sm:justify-center">
+        <Link
+          href="/"
+          className={`hover:text-red-600 font-bold block leading-none tracking-tight duration-200 ease-in-out transition-colors`}
+        >
+          <GlobeIcon />
+        </Link>
       </h1>
 
-      <ul className="flex flex-col content-start tracking-tight">
+      <ul className="flex flex-col max-sm:items-center max-sm:mb-8 content-start tracking-tight">
         {albums.map(album => {
-          const isActive =
-            slug.replace(/%20/g, ' ').toLowerCase() ===
-            album.title.toLowerCase();
+          const isActive = title.toLowerCase() === album.title.toLowerCase();
           return (
             <li key={album.title} className="max-w-fit">
               <Link
                 href={`/${album.title.toLowerCase()}`}
-                className={[
-                  'hover:text-gray-500',
-                  isActive ? 'font-medium' : ''
-                ].join(' ')}
+                className={isActive ? 'font-bold' : 'hover:text-gray-500'}
                 prefetch={false}
               >
                 {album.title}
@@ -33,6 +33,30 @@ export const Nav: React.FC<{ slug: string; albums: AlbumList }> = ({
             </li>
           );
         })}
+        {title && (
+          <>
+            <li className="sm:mt-10 flex gap-1">
+              <Link
+                href="/about"
+                prefetch={false}
+                className="flex gap-1 items-center text-2xl sm:text-sm text-gray-400 hover:text-gray-500"
+              >
+                <InfoIcon />
+                About
+              </Link>
+            </li>
+            <li className="flex sm:mt-1 gap-1 max-sm:hidden">
+              <Link
+                href="/about"
+                prefetch={false}
+                className="flex gap-1 items-center text-lg sm:text-sm text-gray-400 hover:text-gray-500"
+              >
+                <SocialIcon />
+                Socials
+              </Link>
+            </li>
+          </>
+        )}
       </ul>
     </nav>
   );
