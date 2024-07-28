e2b9573e97938735a1342d7e21d3e117cc936cc3
Aaron Agarunov <agarunov.aaron@gmail.com>
2024-07-28 14:57:33 -0400

    Only match the first year found in filename

--- src/lib/api/client.ts
+++ src/lib/api/client.ts
@@ -124,6 +124,18 @@ query($tag: String!) {
       body: JSON.stringify({ query, variables: { tag } }),
       next: { tags: ['photos'] }
     });
+    // If the tag is a year, we might accidentally match 4-digit numbers in the filename that aren't the year.
+    // For example, `IMG_20240211_020123.jpg` is dated in `2024`, but it would show up in our Contentful query for `2012`.
+    // Contentful's GraphQL API does not support filtering by regular expressions.
+    if (tag.match(/2\d{3}/) && response.success) {
+      response.data.assetCollection.items =
+        response.data.assetCollection.items.filter(asset => {
+          const urlParts = asset.url.split('/');
+          const filename = urlParts[urlParts.length - 1];
+          const year = filename.match(/2\d{3}/)?.[0]; // Assume the first 4-digit number is a year
+          return year == tag;
+        });
+    }
     return response;
   }
 }
